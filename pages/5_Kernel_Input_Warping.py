1b6d56d83c425ba96b83c7146590cfca3f30b4fc
Louis Tiao <louistiao@gmail.com>
2024-05-21 15:44:06 +0100

    added controls

--- pages/5_Kernel_Input_Warping.py
+++ pages/5_Kernel_Input_Warping.py
@@ -19,8 +19,6 @@ rc = {
     # "figure.dpi": dpi,
     # "font.serif": ["Palatino", "Times", "Computer Modern"],
     "text.usetex": True,
-    "text.latex.preamble": r"\usepackage{bm}" 
-                           r"\usepackage{nicefrac}",
     # "savefig.dpi": dpi,
     "savefig.transparent": True,
     # "axes.facecolor": (0, 0, 0, 0)
@@ -31,8 +29,9 @@ sns.set_theme(  # context=context, style=style, palette=palette,
     rc=rc
 )
 
-N_TEST = 1 << 9
-N_ROWS = 8
+LOG2_N_TEST_VALUE = 9
+N_ROWS_VALUE = 6
+N_SAMPLES_VALUE = 5
 
 OBSERVATION_NOISE_VARIANCE = 0.
 JITTER = 1e-9
@@ -48,17 +47,17 @@ kernels = tfp.math.psd_kernels
 
 
 kernel_classes = dict(
-    sqrexp=kernels.ExponentiatedQuadratic,
     matern12=kernels.MaternOneHalf,
     matern32=kernels.MaternThreeHalves,
     matern52=kernels.MaternFiveHalves,
+    sqrexp=kernels.ExponentiatedQuadratic,
 )
 
 kernel_names = dict(
-    sqrexp="Squared Exponential",
     matern12="Matern-1/2",
     matern32="Matern-3/2",
     matern52="Matern-5/2",
+    sqrexp="Squared Exponential",
 )
 
 
@@ -69,124 +68,134 @@ def default_float():
 st.set_page_config(page_title="Input Warping", page_icon="🌍")
 
 # st.sidebar.header("Kernel ")
+# st.markdown(
+#     """
+#     Input warping is an augmentation technique used in Gaussian processes (GPs) 
+#     to model nonstationary data by transforming the input space such that 
+#     regions of rapid change can be effectively captured. 
+#     This is particularly useful when the underlying function exhibits varying 
+#     degrees of smoothness or different behaviors across its domain. 
 
-st.markdown(
-    """
-    Input warping is an augmentation technique used in Gaussian processes (GPs) 
-    to model nonstationary data by transforming the input space such that 
-    regions of rapid change can be effectively captured. 
-    This is particularly useful when the underlying function exhibits varying 
-    degrees of smoothness or different behaviors across its domain. 
+#     The core idea is to apply a monotonic transformation to the inputs using 
+#     a cumulative distribution function (CDF), which can warp the inputs 
+#     non-linearly and thereby adapt the GP to the nonstationarities in the data.
 
-    The core idea is to apply a monotonic transformation to the inputs using 
-    a cumulative distribution function (CDF), which can warp the inputs 
-    non-linearly and thereby adapt the GP to the nonstationarities in the data.
+#     A common choice for the transformation is the CDF of the Beta or 
+#     Kumaraswamy distribution, both of which are flexible in shaping the warping 
+#     through their parameters. 
 
-    A common choice for the transformation is the CDF of the Beta or 
-    Kumaraswamy distribution, both of which are flexible in shaping the warping 
-    through their parameters. 
+#     For instance, using the Kumaraswamy CDF 
 
-    For instance, using the Kumaraswamy CDF 
+#     $$
+#     w(x; a, b) = 1 - (1-x^a)^b
+#     $$ 
 
-    $$
-    w(x; a, b) = 1 - (1-x^a)^b
-    $$ 
+#     where $a$ and $b$ are shape parameters, allows for different degrees of 
+#     input compression or expansion. 
 
-    where $a$ and $b$ are shape parameters, allows for different degrees of 
-    input compression or expansion. 
+#     This transformation alters the effective "length-scale" locally, enabling 
+#     the GP to vary its sensitivity to changes in different regions of the 
+#     input space. 
+#     By using such transformations, GPs can better model functions that are 
+#     smooth in some areas but erratic in others, overcoming a traditional 
+#     limitation in handling nonstationary behaviors within a unified modeling 
+#     framework.
+#     """
+# )
 
-    This transformation alters the effective "length-scale" locally, enabling 
-    the GP to vary its sensitivity to changes in different regions of the 
-    input space. 
-    By using such transformations, GPs can better model functions that are 
-    smooth in some areas but erratic in others, overcoming a traditional 
-    limitation in handling nonstationary behaviors within a unified modeling 
-    framework.
-    """
-)
+with st.sidebar:
 
-x1 = np.linspace(0., 1., N_TEST)
-x2 = np.linspace(0., 1., N_ROWS)
+    with st.expander("Base kernel", expanded=False):
+
+        kernel_key = st.radio("kernel $k(x, x')$", 
+                            options=kernel_names.keys(),
+                            index=len(kernel_names)-1,
+                            format_func=kernel_names.get)
+
+        inverse_length_scale = tf.convert_to_tensor(
+            .1 ** st.number_input("$\\log_{10}$ lengthscale", value=-1.2, step=.1),
+            dtype=default_float()
+        )
+        amplitude = tf.convert_to_tensor(
+            10. ** st.number_input("$\\log_{10}$ amplitude", value=0., step=.1),
+            dtype=default_float()
+        )
+
+    with st.expander("Warping function", expanded=True):
+
+    # st.header("")
+    # st.subheader("Kumaraswamy CDF")
+
+        ln_shape_1 = tf.convert_to_tensor(
+            st.number_input("$\\ln$ shape $a$", value=1., step=.05),
+            dtype=default_float()
+        )
+        ln_shape_0 = tf.convert_to_tensor(
+            st.number_input("$\\ln$ shape $b$", value=1., step=.05),
+            dtype=default_float()
+        )
+
+        st.latex(
+            f""" 
+            w(x) = 1 - (1-x^{{{np.exp(ln_shape_1):.3f}}})^{{{np.exp(ln_shape_0):.3f}}}
+            """
+        )
+
+    with st.expander("Advanced options"):
+        n_rows = st.number_input("number of rows", min_value=1, value=N_ROWS_VALUE, step=1)
+        n_test = int(1 << st.number_input("$\\log_2$ number of test points", min_value=0, value=LOG2_N_TEST_VALUE, step=1))
+        n_samples = st.number_input("number of samples", min_value=1, max_value=10, value=N_SAMPLES_VALUE, step=1)
+
+    st.markdown("A dashboard :bar_chart: built :hammer_and_wrench: by [Louis Tiao](https://tiao.io/) " 
+                "([Twitter/X @louistiao](https://x.com/louistiao), " 
+                "[GitHub @ltiao](https://github.com/ltiao))")
+
+x1 = np.linspace(0., 1., n_test)
+x2 = np.linspace(0., 1., n_rows)
 
 X1 = x1[..., np.newaxis]
 X2 = x2[..., np.newaxis]
 
-with st.sidebar:
-    kernel_key = st.radio("Base kernel $k(x, x')$", 
-                          options=kernel_names.keys(),
-                          format_func=kernel_names.get)
 
-    log10_length_scale_value = tf.convert_to_tensor(
-        st.number_input("lengthscale ($\\log_{10}{\\ell}$)", value=-1., step=.1),
-        dtype=default_float()
-    )
-    log10_amplitude_value = tf.convert_to_tensor(
-        st.number_input("amplitude ($\\log_{10}{\\alpha}$)", value=0., step=.1),
-        dtype=default_float()
-    )
+kernel_class = kernel_classes[kernel_key]
+kernel = kernel_class(
+    inverse_length_scale=inverse_length_scale, 
+    amplitude=amplitude,
+)
 
-    st.divider()
-    st.header("Warping Function")
-    st.subheader("Kumaraswamy CDF")
+kernel_warped = tfp.math.psd_kernels.KumaraswamyTransformed(
+    kernel,
+    concentration1=tf.exp(ln_shape_1),
+    concentration0=tf.exp(ln_shape_0),
+)
 
-    ln_concentration1_value = tf.convert_to_tensor(
-        st.number_input("shape ($\\ln{a}$)", value=0., step=.25),
-        dtype=default_float()
-    )
-    ln_concentration0_value = tf.convert_to_tensor(
-        st.number_input("shape ($\\ln{b}$)", value=0., step=.25),
-        dtype=default_float()
-    )
+logger.info(f"Kernel (base): {kernel_warped._kernel}")
+logger.info(f"Kernel (input warped): {kernel_warped}")
+logger.info(f"Warping function: {kernel_warped._transformation_fn}")
 
-    st.latex(
-        f""" 
-        w(x) = 1 - (1-x^{{{np.exp(ln_concentration1_value):.3f}}})^{{{np.exp(ln_concentration0_value):.3f}}}
-        """
-    )
+warping_fn = partial(kernel_warped._transformation_fn,
+                        feature_ndims=1, example_ndims=1)
 
-    kernel_class = kernel_classes[kernel_key]
-    kernel = kernel_class(
-        inverse_length_scale=.1**log10_length_scale_value,
-        amplitude=10.**log10_amplitude_value,
-    )
+logger.info(f"warping_fn(x1): {warping_fn(x1).shape}")
 
-    kernel_warped = tfp.math.psd_kernels.KumaraswamyTransformed(
-        kernel,
-        concentration1=tf.exp(ln_concentration1_value),
-        concentration0=tf.exp(ln_concentration0_value),
+# bijector = tfp.bijectors.KumaraswamyCDF(
+#     concentration1=tf.exp(ln_concentration1_value),
+#     concentration0=tf.exp(ln_concentration0_value),
+# )
+
+data = pd.DataFrame({'x': x1, 'w(x)': warping_fn(x1)})
+
+warping_chart = alt.Chart(data) \
+    .mark_line() \
+    .encode(
+        alt.X('x:Q'), 
+        alt.Y('w(x):Q'),
+    ).properties(
+        height=200,
+        width=200
     )
 
-    logger.info(f"Kernel (base): {kernel_warped._kernel}")
-    logger.info(f"Kernel (input warped): {kernel_warped}")
-    logger.info(f"Warping function: {kernel_warped._transformation_fn}")
-
-    warping_fn = partial(kernel_warped._transformation_fn,
-                         feature_ndims=1, example_ndims=1)
-
-    logger.info(f"warping_fn(x1): {warping_fn(x1).shape}")
-
-    # bijector = tfp.bijectors.KumaraswamyCDF(
-    #     concentration1=tf.exp(ln_concentration1_value),
-    #     concentration0=tf.exp(ln_concentration0_value),
-    # )
-
-    data = pd.DataFrame({'x': x1, 'w(x)': warping_fn(x1)})
-
-    warping_chart = alt.Chart(data) \
-        .mark_line() \
-        .encode(
-            alt.X('x:Q'), 
-            alt.Y('w(x):Q'),
-        ).properties(
-            height=200,
-            width=200
-        )
-
-    # st.altair_chart(chart, use_container_width=True)
-
-    st.divider()
-
-    st.markdown("A demo by [Louis Tiao](https://tiao.io/) (Twitter/X @louistiao, GitHub @ltiao)")
+# st.altair_chart(chart, use_container_width=True)
 
 K = kernel_warped.matrix(X1, X2).numpy()
 
@@ -261,22 +270,20 @@ gaussian_process = tfd.GaussianProcess(
 )
 logger.info(f"Gaussian process: {gaussian_process}")
 
-N_SAMPLES = 3
-
-eps = random_state.randn(N_SAMPLES, N_TEST)
+eps = random_state.randn(n_samples, n_test)
 
 logger.info(f"Normal sample: {eps}")
 
 Y = gaussian_process.get_marginal_distribution() \
     .bijector.forward(eps).numpy()
 
-st.write(Y)
+# st.write(Y)
 
 frames = [pd.DataFrame(dict(sample=i+1, x=x1, y=Y[i])) 
-          for i in range(N_SAMPLES)]
+          for i in range(n_samples)]
 data = pd.concat(frames, axis="index", sort=True)
 
-st.write(data)
+# st.write(data)
 
 samples_chart = alt.Chart(data) \
     .mark_line(clip=True) \
